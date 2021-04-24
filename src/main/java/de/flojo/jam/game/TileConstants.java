b61ddc7288c03d51cff6cb44d3f70bf0903afbec
EagleoutIce <maeusepiep99@web.de>
2021-04-24 14:32:37 +0200

    update Tile constants hover color

--- src/main/java/de/flojo/jam/game/TileConstants.java
+++ src/main/java/de/flojo/jam/game/TileConstants.java
@@ -9,7 +9,7 @@ public final class TileConstants {
     public static final int DEFAULT_RADIUS = 30;
     public static final Font NUMBER_FONT = Main.TEXT_NORMAL.deriveFont(20f);
     public static final Color HIGHLIGHT_COLOR = new Color(0.6f, 0.6f, 0.3f, 0.25f);
-    public static final Color MARK_COLOR = new Color(0.3f, 0.45f, 0.3f, 0.325f);
+    public static final Color MARK_COLOR = new Color(0.3f, 0.45f, 0.3f, 0.475f);
     public static final Color DEFAULT_COLOR = new Color(0.4f, 0.6f, 0.3f, 0.15f);
     public static final Color NONE_COLOR = new Color(0, 0, 0, 35); // 154, 215, 45
     public static final Color P1_COLOR = new Color(45, 173, 215, 35);
